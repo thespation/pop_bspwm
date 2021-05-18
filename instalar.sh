5d93d6ee37b6f3b6276e800fdcc11a710a7016d8
thespation <84329097+thespation@users.noreply.github.com>
2021-05-18 14:35:36 -0300

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -7,41 +7,38 @@
 
 clear
 
-echo "Atualizando sistema"
-sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade -y
+echo "########## Atualizando sistema ##########"
+sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y
 
-echo "Instalando base BSPWM (bspwm, polybar, sxhkd, rofi e mate-polkit)"
-sudo apt install -y bspwm polybar sxhkd rofi mate-polkit
+echo "########## Instalando base BSPWM (bspwm, polybar, sxhkd, rofi e mate-polkit) ##########m"
+sudo apt install bspwm polybar sxhkd rofi mate-polkit -y
 
-echo "Instalando aplicativos essenciais (nitrogen, lxappearance e dmenu)"
-sudo apt install -y nitrogen lxappearance dmenu
+echo "########## Instalando aplicativos essenciais (nitrogen, lxappearance e dmenu) ##########"
+sudo apt install nitrogen lxappearance dmenu -y
 
-echo "Aplicativos opcionais (neofetch, htop scrot e compton)"
-sudo apt install -y neofetch htop scrot compton
+echo "########## Aplicativos opcionais (neofetch, htop scrot e compton) ##########"
+sudo apt install neofetch htop scrot compton -y
 
-echo "Habilitar rofi na tecla super"
-sudo apt install -y snapd && sudo snap install ksuperkey
+echo "########## Habilitar rofi na tecla super ##########"
+sudo apt install snapd -y && sudo snap install ksuperkey
 
-echo "Copiando configurações do GitHub"
-cd ~/Downloads && git clone https://github.com/thespation/pop_bspwm
-
-echo "Criar diretório de fonts, caso não exista"
+echo "########## Criar diretório de fonts, caso não exista ##########"
 mkdir ~/.fonts 
 
-echo "Fontes necessárias copiadas"
-cp -r ~/Downloads/pop_bspwm/fonts/* ~/.fonts
+echo "########## Fontes necessárias copiadas ##########"
+cp -r /pop_bspwm/fonts/* ~/.fonts
 
-echo "Pasta polybar copiada"
-cp -r ~/Downloads/pop_bspwm/polybar ~/.config && cd ~/.config/polybar && chmod 755 ~/.config/polybar/* -R
+echo "########## Pasta polybar copiada ##########"
+cp -r /pop_bspwm/polybar ~/.config && cd ~/.config/polybar && chmod 755 ~/.config/polybar/* -R
 
-echo "Pasta rofi copiada"
-cp -r ~/Downloads/pop_bspwm/rofi ~/.config && cd ~/.config/rofi && chmod 755 ~/.config/rofi/* -R
+echo "########## Pasta rofi copiada ##########"
+cp -r /pop_bspwm/rofi ~/.config && cd ~/.config/rofi && chmod 755 ~/.config/rofi/* -R
 
-echo "Pasta sxhkd copiada"
-cp -r ~/Downloads/pop_bspwm/sxhkd ~/.config && cd ~/.config/sxhkd && chmod 755 ~/.config/sxhkd/* -R
+echo "########## Pasta sxhkd copiada ##########"
+cp -r /pop_bspwm/sxhkd ~/.config && cd ~/.config/sxhkd && chmod 755 ~/.config/sxhkd/* -R
 
-echo "Pasta bspwm copiada"
-cp -r ~/Downloads/pop_bspwm/bspwm ~/.config && cd ~/.config/bspwm && chmod 755 ~/.config/bspwm/* -R
+echo "########## Pasta bspwm copiada ##########"
+cp -r /pop_bspwm/bspwm ~/.config && cd ~/.config/bspwm && chmod 755 ~/.config/bspwm/* -R
 
-echo "Removendo botões de janela e aplicando tema PopOs"
-cp -rf ~/Downloads/pop_bspwm/gtk-3.0/ ~/.config/ && chmod 755 ~/.config/gtk-3.0/* -R
+echo "########## Removendo botões de janela e aplicando tema PopOs ##########"
+cp -rf /pop_bspwm/gtk-3.0/ ~/.config/ && chmod 755 ~/.config/gtk-3.0/* -R
