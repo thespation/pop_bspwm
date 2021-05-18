cd932de2f740cae97fbd0c3c66731571900dcbcd
thespation <84329097+thespation@users.noreply.github.com>
2021-05-18 11:04:37 -0300

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -17,10 +17,10 @@ echo "Instalando aplicativos essenciais (nitrogen, lxappearance e dmenu)"
 sudo apt install -y nitrogen lxappearance dmenu
 
 echo "Aplicativos opcionais (neofetch, htop scrot e compton)"
-sudo apt install neofetch htop scrot compton
+sudo apt install -y neofetch htop scrot compton
 
 echo "Habilitar rofi na tecla super"
-sudo apt install snapd && sudo snap install ksuperkey
+sudo apt install -y snapd && sudo snap install ksuperkey
 
 echo "Copiando configurações do GitHub"
 cd ~/Downloads && git clone https://github.com/thespation/pop_bspwm
@@ -44,5 +44,4 @@ echo "Pasta bspwm copiada"
 cp -r ~/Downloads/pop_bspwm/bspwm ~/.config && cd ~/.config/bspwm && chmod 755 ~/.config/bspwm/* -R
 
 echo "Removendo botões de janela e aplicando tema PopOs"
-mv ~/.config/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini.old
-cp -r ~/Downloads/pop_bspwm/gtk-3.0/settings.ini ~/.config/gtk-3.0/ && chmod -R a+rX *
+mkdir ~/.config/gtk-3.0 && cp -rf ~/Downloads/pop_bspwm/gtk-3.0/settings.ini ~/.config/gtk-3.0/ && chmod 755 ~/.config/gtk-3.0/* -R
