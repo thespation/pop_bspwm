f0ea31793122c8b7b71eff08999acb56260549fc
William Santos <thespation@gmail.com>
2021-06-03 21:43:12 -0300

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -1,44 +1,82 @@
 #!/usr/bin/env bash
-# script: PopOs BSPWM
+# script: BSPWM no PopOs
 # descrição: Instalar BSPWM no Pop!_OS
 # autor: William Santos
 # site: https://github.com/thespation/pop_bspwm
-# version: 1
+# version: 2
+
+set -e
+
+## Atualização de sistema ##
+clear
+	echo "#-----------------------------Atualizar Sistema------------------------------#"
+sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y
+
+	clear
+	echo "#----------------------------Sistema atualizado------------------------------#"
+sleep 3s
+
+	echo "#---------------------------Instalando base BSPWM----------------------------#"
+	sudo apt install bspwm polybar sxhkd rofi mate-polkit nitrogen lxappearance dmenu compton -y
+
+	clear
+	echo "#----------------------------Sistema atualizado------------------------------#"
+	echo "#---------------------------Base BSPWM instalada-----------------------------#"
+sleep 3s	
+	echo "#----------------Apps complementares (neofetch, htop e scrot)----------------#"
+	sudo apt install neofetch htop scrot -y
+
+	clear
+	echo "#----------------------------Sistema atualizado------------------------------#"
+	echo "#---------------------------Base BSPWM instalada-----------------------------#"
+	echo "#----------------------Apps complementares instalados------------------------#"
+sleep 3s
 
 clear
+	echo "#----------------------------Sistema atualizado------------------------------#"
+	echo "#---------------------------Base BSPWM instalada-----------------------------#"	
+	echo "#----------------------Apps complementares instalados------------------------#"
+sleep 3s
+	echo "#------------------------Habilitar snap e ksuperkey--------------------------#"
+	sudo apt install snapd -y && sudo snap install ksuperkey
 
-echo "########## Atualizando sistema ##########"
-sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y
 
-echo "########## Instalando base BSPWM (bspwm, polybar, sxhkd, rofi e mate-polkit) ##########"
-sudo apt install bspwm polybar sxhkd rofi mate-polkit -y
+	clear
+	echo "#----------------------------Sistema atualizado------------------------------#"
+	echo "#---------------------------Base BSPWM instalada-----------------------------#"	
+	echo "#----------------------Apps complementares instalados------------------------#"
+	echo "#------------------Snap e ksuperkey habilitados com sucesso------------------#"
+sleep 3s
+	echo "#---------------Copiar fontes necessárias para exibir ícones-----------------#"
+	sudo cp -r /tmp/pop_bspwm/fonts/* /usr/share/fonts
+	
+	clear
+	echo "#----------------------------Sistema atualizado------------------------------#"
+	echo "#---------------------------Base BSPWM instalada-----------------------------#"	
+	echo "#----------------------Apps complementares instalados------------------------#"
+	echo "#------------------Snap e ksuperkey habilitados com sucesso------------------#"
+	echo "#-----------------------------Fontes copiadas--------------------------------#"
+sleep 3s	
+	echo "#--------------------------Copiar personalizações----------------------------#"
+	cp -r /tmp/pop_bspwm/bspwm ~/.config
+	cp -r /tmp/pop_bspwm/sxhkd ~/.config
+	cp -r /tmp/pop_bspwm/polybar ~/.config
+	cp -r /tmp/pop_bspwm/rofi ~/.config
+	cp -rf /tmp/pop_bspwm/gtk-3.0/ ~/.config/
 
-echo "########## Instalando aplicativos essenciais (nitrogen, lxappearance e dmenu) ##########"
-sudo apt install nitrogen lxappearance dmenu -y
-
-echo "########## Aplicativos complementares (neofetch, htop scrot e compton) ##########"
-sudo apt install neofetch htop scrot compton -y
-
-echo "########## Habilitar rofi na tecla super ##########"
-sudo apt install snapd -y && sudo snap install ksuperkey
-
-echo "########## Criar diretório de fonts, caso não exista ##########"
-mkdir ~/.fonts 
-
-echo "########## Fontes necessárias copiadas ##########"
-cp -r /tmp/pop_bspwm/fonts/* ~/.fonts
-
-echo "########## Pasta polybar copiada ##########"
-cp -r /tmp/pop_bspwm/polybar ~/.config
-
-echo "########## Pasta rofi copiada ##########"
-cp -r /tmp/pop_bspwm/rofi ~/.config
-
-echo "########## Pasta sxhkd copiada ##########"
-cp -r /tmp/pop_bspwm/sxhkd ~/.config
-
-echo "########## Pasta bspwm copiada ##########"
-cp -r /tmp/pop_bspwm/bspwm ~/.config
-
-echo "########## Removendo botões de janela e aplicando tema PopOs ##########"
-cp -rf /tmp/pop_bspwm/gtk-3.0/ ~/.config/
+sleep 3s
+	
+	clear
+	echo "#----------------------------Sistema atualizado------------------------------#"
+	echo "#---------------------------Base BSPWM instalada-----------------------------#"	
+	echo "#----------------------Apps complementares instalados------------------------#"
+	echo "#------------------Snap e ksuperkey habilitados com sucesso------------------#"
+	echo "#-----------------------------Fontes copiadas--------------------------------#"
+	echo "#------Personalizações copiadas (bspwm, sxhkd, polybar, rofi e tema gtk------#"
+	echo "#---Para que tudo funcione corretamente é necessário reiniciar o seu micro---#"
+	echo "#                                                                            #"
+	echo "##############################################################################"
+	echo " "
+	echo "Seu computador reiniciará em 1 minuto, caso tenha alguma coisa importante aberta sem salvar, salve para não perder informação."
+sleep 1m
+	reboot
