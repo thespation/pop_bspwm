475331c6f7bac74ca30542929e16b911b0c64acb
William Santos <thespation@gmail.com>
2021-07-01 16:33:48 -0300

    Add files via upload

---
+++ bspwm/rofi/bin/powermenu.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+# Available Styles
+# >> Created and tested on : rofi 1.6.0-1
+#
+
+## Default
+DIR="~/.config/bspwm/rofi/themes"
+STYLE="powermenu"
+
+rofi_command="rofi -theme $DIR/$STYLE.rasi"
+
+uptime=$(uptime -p | sed -e 's/up //g')
+
+# Options
+if [[ "$DIR" == "powermenus" ]]; then
+	shutdown=""
+	reboot=""
+	lock=""
+	suspend=""
+	logout=""
+	ddir="$HOME/.config/bspwm/rofi/bin"
+else
+	shutdown=" Desligar"
+	reboot=" Reiniciar"
+	lock=" Bloquear"
+	suspend=" Em espera"
+	logout=" Sair"
+	ddir="$HOME/.config/rofi/dialogs"
+fi
+
+rdialog () {
+rofi -dmenu\
+    -i\
+    -no-fixed-num-lines\
+    -p "Tem certeza? : "\
+    -theme "$ddir/confirm.rasi"
+}
+
+# Variable passed to rofi
+MSG="Opções : sim / nao / s / n"
+options="$lock\n$suspend\n$logout\n$reboot\n$shutdown"
+
+chosen="$(echo -e "$options" | $rofi_command -p "Tempo ligado - $uptime" -dmenu -selected-row 0)"
+case $chosen in
+    $shutdown)
+        systemctl poweroff
+        ;;
+    $reboot)
+        systemctl reboot
+        ;;
+    $lock)
+        xflock4
+        ;;
+    $suspend)
+        systemctl suspend
+        ;;
+    $logout)
+        bspc quit
+
+        ;;
+esac
+
---
+++ instalar.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+# script: BSPWM no PopOs
+# descrição: Instalar BSPWM no Pop!_OS
+# autor: William Santos
+# site: https://github.com/thespation/pop_bspwm
+# version: 2
+
+# set -e
+
+## Atualização de sistema ##
+clear
+	echo "#-----------------------------Atualizar Sistema------------------------------#"
+sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y
+
+	clear
+	echo "#----------------------------Sistema atualizado------------------------------#"
+sleep 3s
+
+	echo "#---------------------------Instalando base BSPWM----------------------------#"
+	sudo apt install bspwm polybar sxhkd rofi mate-polkit nitrogen lxappearance compton -y
+
+	clear
+	echo "#----------------------------Sistema atualizado------------------------------#"
+	echo "#---------------------------Base BSPWM instalada-----------------------------#"
+sleep 3s	
+	echo "#----------------Apps complementares (neofetch, htop e scrot)----------------#"
+	sudo apt install neofetch htop -y
+
+	clear
+	echo "#----------------------------Sistema atualizado------------------------------#"
+	echo "#---------------------------Base BSPWM instalada-----------------------------#"
+	echo "#----------------------Apps complementares instalados------------------------#"
+sleep 3s
+
+clear
+	echo "#----------------------------Sistema atualizado------------------------------#"
+	echo "#---------------------------Base BSPWM instalada-----------------------------#"	
+	echo "#----------------------Apps complementares instalados------------------------#"
+sleep 3s
+	echo "#------------------------Habilitar snap e ksuperkey--------------------------#"
+	sudo apt install snapd -y && sudo snap install ksuperkey
+
+
+	clear
+	echo "#----------------------------Sistema atualizado------------------------------#"
+	echo "#---------------------------Base BSPWM instalada-----------------------------#"	
+	echo "#----------------------Apps complementares instalados------------------------#"
+	echo "#------------------Snap e ksuperkey habilitados com sucesso------------------#"
+sleep 3s
+	echo "#---------------Copiar fontes necessárias para exibir ícones-----------------#"
+	sudo cp -r /tmp/pop_bspwm/fonts/* /usr/share/fonts
+	
+	clear
+	echo "#----------------------------Sistema atualizado------------------------------#"
+	echo "#---------------------------Base BSPWM instalada-----------------------------#"	
+	echo "#----------------------Apps complementares instalados------------------------#"
+	echo "#------------------Snap e ksuperkey habilitados com sucesso------------------#"
+	echo "#-----------------------------Fontes copiadas--------------------------------#"
+sleep 3s	
+	echo "#--------------------------Copiar personalizações----------------------------#"
+	cp -r /tmp/pop_bspwm/bspwm ~/.config
+	cp -r /tmp/pop_bspwm/sxhkd ~/.config
+	cp -rf /tmp/pop_bspwm/gtk-3.0/ ~/.config/
+
+sleep 3s
+	
+	clear
+	echo "#----------------------------Sistema atualizado------------------------------#"
+	echo "#---------------------------Base BSPWM instalada-----------------------------#"	
+	echo "#----------------------Apps complementares instalados------------------------#"
+	echo "#------------------Snap e ksuperkey habilitados com sucesso------------------#"
+	echo "#-----------------------------Fontes copiadas--------------------------------#"
+	echo "#------Personalizações copiadas (bspwm, sxhkd, polybar, rofi e tema gtk------#"
+	echo "#---Para que tudo funcione corretamente é necessário reiniciar o seu micro---#"
+	echo "#                                                                            #"
+	echo "##############################################################################"
+	echo " "
+	echo "Seu computador reiniciará em 1 minuto, caso tenha alguma coisa importante aberta sem salvar, salve para não perder informação."
+sleep 1m
+	reboot
